e4e63a78afa605371a968a80c0962c1b653587ad
semenenko <semenenko_myk@ukr.net>
2023-04-16 21:41:38 +0300

    working scatch for apds9960. it works with i2c direct connection. apds9960 sensor is equipped with 5V ldo.

--- Arduino_platform/Arduino_platform.ino
+++ Arduino_platform/Arduino_platform.ino
@@ -1,12 +1,88 @@
+/***************************************************************************
+  This is a 
+
+  This sketch puts the sensor in color mode and reads the RGB and clear values.
+
+  Designed specifically to work with the 
+
+
+  Written by  for .
+  BSD license, all text above must be included in any redistribution
+ ***************************************************************************/
+
 #include "packuart.h"
 #include "mega_mgr.h"
+#include "Adafruit_APDS9960.h"
+
+//
+void validSerialData(void);
+void getSerialData(void);
+void initAPDS9960(void);
+void getAPDS9960(void);
+//
+
+quint8 buff[BUFF_LENGTH];
+PackUART packerUART;
+uint16_t r;
+uint16_t g;
+uint16_t b;
+uint16_t c;
+MegaMng megaMng;
+Adafruit_APDS9960 apds;
 
 void setup() 
 {
+  
   Serial.begin(9600);
+  Serial.println("INFO: SERIAL=9600//");
+
+//
+  initAPDS9960();
+//
+
 }
 
-quint8 buff[BUFF_LENGTH];
+void loop() 
+{
+  
+  validSerialData(); //****************FSM********************
+
+  getAPDS9960();
+  
+}
+
+
+//**********************Framework***********************
+void validSerialData()
+{
+    if (Serial.available() > 0) 
+    {
+        getSerialData();
+        
+        if (!packerUART.isPacketStructureCorrect(buff))
+        {
+            Serial.print("ERROR: Packet structure invalid//");
+        }
+        else
+        {
+            Serial.print("INFO: Packet structure valid//");
+            
+            MM_EVENT event = megaMng.checkInstructionType(buff);
+            
+            if (event != MM_EVENT::LAST_EVENT)
+            {
+                //****************FSM********************
+                megaMng.executeFSM(event);
+                //***************************************
+            }
+            else
+            {
+                Serial.print("ERROR: Invalid event received//");
+            }
+        }
+    }
+    
+}
 
 void getSerialData()
 {  
@@ -15,30 +91,44 @@ void getSerialData()
     Serial.print("INFO: DATA COLLECTED SUCCESSFUL//");
 }
 
-PackUART packerUART;
-MegaMng megaMng;
-
-void loop() 
+void initAPDS9960()
 {
-    if (Serial.available() > 0) 
-    {
-        getSerialData();
-        if (!packerUART.isPacketStructureCorrect(buff))
-        {
-            Serial.print("ERROR: Packet structure invalid//");
-        }
-        else
-        {
-            Serial.print("INFO: Packet structure valid//");
-            MM_EVENT event = megaMng.checkInstructionType(buff);
-            if (event != MM_EVENT::LAST_EVENT)
-            {
-                megaMng.executeFSM(event);
-            }
-            else
-            {
-                Serial.print("ERROR: Invalid event received//");
-            }
-        }
-    }
+  if (!apds.begin())
+  {
+    Serial.println("failed to initialize device! Please check your wiring.");
+  }
+  else
+  {
+    Serial.println("Device initialized!");
+  }
+  
+  apds.enableColor(true); // enable color sensign mode
+}
+
+void getAPDS9960()
+{
+  // wait for color data to be ready
+  while(!apds.colorDataReady())
+  {
+    delay(5);
+  }
+
+  // get the data and print the different channels
+  apds.getColorData(&r, &g, &b, &c);
+  
+  Serial.print("red: ");
+  Serial.print(r);
+  
+  Serial.print(" green: ");
+  Serial.print(g);
+  
+  Serial.print(" blue: ");
+  Serial.print(b);
+  
+  Serial.print(" clear: ");
+  
+  Serial.println(c);
+  Serial.println();
+  
+  delay(1000);
 }
